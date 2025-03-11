8af71022f98d3ffa914032be4ecd6bc7bd19a2e4
SSAYKO <sandresmurillo@outlook.com>
2025-03-11 14:49:11 -0500

    Add user icon SVG and update UI components with new styles and fonts

--- devinsight/lib/ui/login/screens/loginScreen.dart
+++ devinsight/lib/ui/login/screens/loginScreen.dart
@@ -11,11 +11,14 @@ class LoginScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
-            colors: [Colors.grey.shade800, Colors.black],
+            colors: [
+              Color.fromARGB(255, 20, 6, 48),
+              Color.fromARGB(255, 4, 1, 9)
+            ],
           ),
         ),
         child: Center(
@@ -24,27 +27,32 @@ class LoginScreen extends ConsumerWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Icon(Icons.code, color: Colors.green, size: 80),
+                const Icon(Icons.code, color: Color(0xFF1ABCFE), size: 80),
                 const SizedBox(height: 10),
                 const Text(
                   "DevInsight",
                   style: TextStyle(
-                    color: Colors.green,
+                    color: Color(0xFF1ABCFE),
                     fontSize: 22,
+                    fontFamily: "Montserrat",
                     fontWeight: FontWeight.bold,
                   ),
                 ),
                 const SizedBox(height: 40),
                 const CustomTextField(label: "Usuario", icon: Icons.person),
                 const SizedBox(height: 20),
-                const CustomTextField(label: "Contraseña", icon: Icons.lock, isPassword: true),
+                const CustomTextField(
+                    label: "Contraseña", icon: Icons.lock, isPassword: true),
                 Align(
                   alignment: Alignment.centerRight,
                   child: TextButton(
                     onPressed: () {},
                     child: const Text(
                       "¿Olvidaste tu contraseña?",
-                      style: TextStyle(color: Colors.green),
+                      style: TextStyle(
+                          color: Color(0xFF1ABCFE),
+                          fontFamily: "Montserrat",
+                          fontWeight: FontWeight.bold),
                     ),
                   ),
                 ),
@@ -59,9 +67,19 @@ class LoginScreen extends ConsumerWidget {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    SocialButton(icon: Icons.code, onPressed: () => print("GitHub")),
+                    Expanded(
+                      child: SocialButton(
+                        icon: Icons.code,
+                        onPressed: () => print("GitHub"),
+                      ),
+                    ),
                     const SizedBox(width: 20),
-                    SocialButton(icon: Icons.facebook, onPressed: () => print("Facebook")),
+                    Expanded(
+                      child: SocialButton(
+                        icon: Icons.facebook,
+                        onPressed: () => print("Facebook"),
+                      ),
+                    ),
                   ],
                 ),
                 const SizedBox(height: 20),
@@ -73,6 +91,7 @@ class LoginScreen extends ConsumerWidget {
                       color: Colors.white,
                       fontWeight: FontWeight.bold,
                       fontSize: 14,
+                      fontFamily: "Montserrat",
                     ),
                   ),
                 ),
--- devinsight/lib/ui/login/widgets/socialButton.dart
+++ devinsight/lib/ui/login/widgets/socialButton.dart
@@ -5,22 +5,25 @@ class SocialButton extends StatelessWidget {
   final VoidCallback onPressed;
 
   const SocialButton({
-    Key? key,
+    super.key,
     required this.icon,
     required this.onPressed,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onPressed,
       child: Container(
-        padding: EdgeInsets.all(10),
+        width: double.infinity,
+        padding: const EdgeInsets.all(10),
         decoration: BoxDecoration(
-          color: Colors.black.withOpacity(0.2),
+          color: const Color(0xFF1F212A),
           borderRadius: BorderRadius.circular(8),
         ),
-        child: Icon(icon, color: Colors.white, size: 30),
+        child: Center(
+          child: Icon(icon, color: Colors.white, size: 30),
+        ),
       ),
     );
   }
