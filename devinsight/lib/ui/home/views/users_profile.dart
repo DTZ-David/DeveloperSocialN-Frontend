c695f15df5d6fa49e13587255ee89102a6d87277
SSAYKO <sandresmurillo@outlook.com>
2025-05-06 13:10:42 -0500

    feat: Update user profile view and followers count display, enhance UI with SVG icons

--- devinsight/lib/ui/home/views/users_profile.dart
+++ devinsight/lib/ui/home/views/users_profile.dart
@@ -1,6 +1,8 @@
 import 'package:devinsight/config/providers/users_provider.dart';
+import 'package:devinsight/ui/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class UsersProfileView extends ConsumerWidget {
   final String userId;
@@ -12,31 +14,265 @@ class UsersProfileView extends ConsumerWidget {
     final userAsync = ref.watch(userByIdProvider(int.parse(userId)));
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('User Profile'),
-      ),
+      appBar: _buildAppBar(),
+      backgroundColor: AppColors.background,
       body: userAsync.when(
-        data: (user) => Padding(
-          padding: const EdgeInsets.all(16.0),
+        loading: () => const Center(child: CircularProgressIndicator()),
+        error: (err, _) => Center(child: Text('Error: $err')),
+        data: (user) => SingleChildScrollView(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              CircleAvatar(
-                backgroundImage: NetworkImage(user.profilePicture),
-                radius: 40,
-              ),
-              const SizedBox(height: 12),
-              Text(user.username,
-                  style: const TextStyle(
-                      fontSize: 20, fontWeight: FontWeight.bold)),
-              const SizedBox(height: 8),
-              Text(user.bio),
+              _ProfileHeader(user: user),
+              const SizedBox(height: 10),
+              
+              _OptionsList(),
+              
             ],
           ),
         ),
-        loading: () => const Center(child: CircularProgressIndicator()),
-        error: (err, _) => Center(child: Text('Error: $err')),
       ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: AppColors.primaryColors,
+      title: const Text(
+        "Perfil",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          fontFamily: 'Montserrat',
+        ),
+      ),
+      actions: const [SizedBox(width: 16)],
+    );
+  }
+}
+
+class _ProfileHeader extends StatelessWidget {
+  final dynamic user;
+
+  const _ProfileHeader({required this.user});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.only(top: 10, bottom: 20),
+      decoration: const BoxDecoration(
+        color: AppColors.primaryColors,
+      ),
+      child: Column(
+        children: [
+          CircleAvatar(
+            radius: 40,
+            backgroundImage: user.profilePicture != null
+                ? NetworkImage(user.profilePicture)
+                : null,
+            child: user.profilePicture == null
+                ? const Icon(Icons.person, size: 40, color: Colors.white)
+                : null,
+          ),
+          const SizedBox(height: 12),
+          Text(
+            user.username ?? 'Unknown',
+            style: const TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+              fontFamily: "Montserrat",
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 12.0),
+            child: Text(
+              user.email ?? 'No email provided',
+              style: const TextStyle(
+                color: Colors.white,
+                fontFamily: "Montserrat",
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _StatCard(
+                title: user.postsCount?.toString() ?? '0',
+                subtitle: 'Posts',
+              ),
+              const SizedBox(width: 20),
+              _StatCard(
+                title: user.followersCount?.toString() ?? '0',
+                subtitle: 'Seguidores',
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const _SocialButtonsRow(
+            buttonText: 'Seguir',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _SocialButtonsRow extends StatelessWidget {
+  final String buttonText;
+
+  const _SocialButtonsRow({required this.buttonText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            // Handle button tap
+            print('Follow Action');
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color(0xFF1ABCFE),
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 8),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+          child: Text(
+            buttonText,
+            style: const TextStyle(
+                color: Colors.white,
+                fontSize: 14,
+                fontFamily: "Montserrat",
+                fontWeight: FontWeight.bold),
+          ),
+        ),
+        const SizedBox(
+          width: 10,
+        ),
+        GestureDetector(
+          onTap: () {
+            // Handle button tap
+            print('Go to message with /id');
+          },
+          child: Container(
+            width: 46,
+            height: 42,
+            padding: const EdgeInsets.all(15),
+            decoration: BoxDecoration(
+              color: const Color(0xFF1F212A),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: SvgPicture.asset(
+              "assets/icons/message_profile.svg",
+              height: 100,
+              width: 100,
+              color: AppColors.accent,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class SocialButton extends StatelessWidget {
+  final String iconPath;
+  final Color color;
+
+  const SocialButton({
+    super.key,
+    required this.iconPath,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // Handle button tap
+        print('Button tapped');
+      },
+      child: Container(
+        width: 48,
+        height: 48,
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          color: const Color(0xFF1F212A),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: SvgPicture.asset(
+          iconPath,
+          height: 40,
+          width: 40,
+          color: color,
+        ),
+      ),
+    );
+  }
+}
+
+class _StatCard extends StatelessWidget {
+  final String title;
+  final String subtitle;
+
+  const _StatCard({
+    required this.title,
+    required this.subtitle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          title,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontFamily: "Montserrat",
+            fontSize: 16,
+          ),
+        ),
+        const SizedBox(width: 6),
+        Text(
+          subtitle,
+          style: const TextStyle(
+            fontSize: 13,
+            color: Colors.white,
+            fontFamily: "Montserrat",
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _OptionsList extends StatelessWidget {
+  const _OptionsList();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListTile(
+          leading: const Icon(Icons.settings),
+          title: const Text("Personal settings"),
+          onTap: () {
+            // Handle settings tap
+          },
+        ),
+        ListTile(
+          leading: const Icon(Icons.menu_book),
+          title: const Text("Our training guide"),
+          onTap: () {
+            // Handle guide tap
+          },
+        ),
+      ],
     );
   }
 }
