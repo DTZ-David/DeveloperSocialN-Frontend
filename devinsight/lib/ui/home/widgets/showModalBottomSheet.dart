16f5717832b04196646dad41952cf5360cc11d47
ozark7 <gotbd7@gmail.com>
2025-05-08 14:21:42 -0500

    feat: Enhance CommentModal with improved layout and confirmation dialog

--- devinsight/lib/ui/home/widgets/showModalBottomSheet.dart
+++ devinsight/lib/ui/home/widgets/showModalBottomSheet.dart
@@ -1,3 +1,5 @@
+
+import 'package:devinsight/ui/home/widgets/interactionsCard.dart';
 import 'package:flutter/material.dart';
 
 class CommentModal {
@@ -7,6 +9,14 @@ class CommentModal {
     final TextEditingController _commentController = TextEditingController();
 
     return await showModalBottomSheet<String>(
+      barrierColor: Colors.white.withOpacity(0.33),
+      enableDrag: false,
+      isDismissible: false,
+      transitionAnimationController: AnimationController(
+        vsync: Navigator.of(context),
+        duration: const Duration(milliseconds: 1000),
+      ),
+      showDragHandle: true,
       context: context,
       isScrollControlled: true,
       backgroundColor: Colors.black,
@@ -14,113 +24,120 @@ class CommentModal {
         borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
       ),
       builder: (context) {
-        return Padding(
-          padding: EdgeInsets.only(
-            bottom: MediaQuery.of(context).viewInsets.bottom,
-            left: 16,
-            right: 16,
-            top: 20,
-          ),
-          child: StatefulBuilder(
-            builder: (context, setState) {
-              return Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  // Botón de cerrar en la parte superior derecha
-                  Align(
-                    alignment: Alignment.topRight,
-                    child: IconButton(
-                      icon: const Icon(
-                        Icons.close,
-                        color: Colors.white,
+        return FractionallySizedBox(
+          heightFactor: 0.9,
+          child: Padding(
+            padding: EdgeInsets.only(
+              bottom: MediaQuery.of(context).viewInsets.bottom,
+              left: 16,
+              right: 16,
+              top: 20,
+            ),
+            child: StatefulBuilder(
+              builder: (context, setState) {
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: IconButton(
+                        icon: const Icon(
+                          Icons.close,
+                          color: Colors.white,
+                        ),
+                        onPressed: () async {
+                          bool discard = await _showConfirmationDialog(context);
+                          if (discard) {
+                            Navigator.pop(context);
+                          }
+                        },
                       ),
-                      onPressed: () async {
-                        // Pide confirmación para descartar el comentario
-                        bool discard = await _showConfirmationDialog(context);
-                        if (discard) {
-                          Navigator.pop(context);
-                        }
-                      },
                     ),
-                  ),
-                  const Text(
-                    'Agregar comentario',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontFamily: 'Montserrat',
+                    const InteractionCard(
+                      tipo: 'Comentarios',
+                      id: 'Comentario',
+                      mensaje:
+                          '¿Qué te parece la nueva funcionalidad de DevInsight?',
                     ),
-                  ),
-                  const SizedBox(height: 12),
-                  ConstrainedBox(
-                    constraints: BoxConstraints(
-                      maxHeight: MediaQuery.of(context).size.height * 0.4,
+                    const Text(
+                      'Agregar comentario',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontFamily: 'Montserrat',
+                      ),
                     ),
-                    child: Scrollbar(
-                      child: SingleChildScrollView(
-                        child: TextField(
-                          controller: _commentController,
-                          keyboardType: TextInputType.multiline,
-                          maxLines: null, // Permite múltiples líneas
-                          style: const TextStyle(color: Colors.white),
-                          decoration: InputDecoration(
-                            hintText: 'Escribe tu comentario...',
-                            hintStyle: const TextStyle(color: Colors.grey),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(12),
-                              borderSide: const BorderSide(color: Colors.black),
+                    const SizedBox(height: 12),
+                    ConstrainedBox(
+                      constraints: BoxConstraints(
+                        maxHeight: MediaQuery.of(context).size.height * 0.4,
+                      ),
+                      child: Scrollbar(
+                        child: SingleChildScrollView(
+                          child: TextField(
+                            controller: _commentController,
+                            keyboardType: TextInputType.multiline,
+                            maxLines: null,
+                            style: const TextStyle(color: Colors.white),
+                            decoration: InputDecoration(
+                              hintText: 'Escribe tu comentario...',
+                              hintStyle: const TextStyle(color: Colors.grey),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12),
+                                borderSide:
+                                    const BorderSide(color: Colors.black),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12),
+                                borderSide:
+                                    const BorderSide(color: Colors.blueAccent),
+                              ),
+                              fillColor: Colors.black,
+                              filled: true,
                             ),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(12),
-                              borderSide:
-                                  const BorderSide(color: Colors.blueAccent),
-                            ),
-                            fillColor: Colors.black,
-                            filled: true,
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 12),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blueAccent,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
+                    const SizedBox(height: 12),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blueAccent,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
                       ),
-                    ),
-                    onPressed: () {
-                      final comment = _commentController.text.trim();
-                      if (comment.isNotEmpty) {
-                        Navigator.pop(context, comment);
-                      }
-                    },
-                    child: const Padding(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: 24.0, vertical: 10.0),
-                      child: Text(
-                        'Publicar',
-                        style: TextStyle(
-                          fontFamily: 'Montserrat',
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
+                      onPressed: () {
+                        final comment = _commentController.text.trim();
+                        if (comment.isNotEmpty) {
+                          Navigator.pop(context, comment);
+                        }
+                      },
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 24.0, vertical: 10.0),
+                        child: Text(
+                          'Publicar',
+                          style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 20),
-                ],
-              );
-            },
+                    const SizedBox(height: 20),
+                  ],
+                );
+              },
+            ),
           ),
         );
       },
     );
   }
 
-  // Función para mostrar el cuadro de confirmación
   static Future<bool> _showConfirmationDialog(BuildContext context) async {
     return await showDialog<bool>(
           context: context,
