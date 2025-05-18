937388938577911062e2ada6aabdbe3e3490ac36
Daniela Paz <79215503+DTZ-David@users.noreply.github.com>
2025-05-18 10:53:59 -0500

    post refactor

--- devinsight/lib/models/publication/post.dart
+++ devinsight/lib/models/publication/post.dart
@@ -1,28 +1,3 @@
-/*import 'reaction.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
-import 'comment.dart';
-part 'post.freezed.dart';
-part 'post.g.dart';
-
-@freezed
-class Post with _$Post {
-  const factory Post({
-    required String authorId,
-    required String codeSnippet,
-    required String description,
-    required String userName,
-    required String profilePicture,
-    required int likes,
-    required List<String> tags,
-    @Default([]) List<Comment> comments,
-    @Default([]) List<Reaction> reactions,
-  }) = _Post;
-
-  factory Post.fromJson(Map<String, dynamic> json) => _$PostFromJson(json);
-}
-*/
-
-import 'package:devinsight/models/publication/comment.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 part 'post.freezed.dart';
 part 'post.g.dart';
@@ -30,11 +5,11 @@ part 'post.g.dart';
 @freezed
 class Post with _$Post {
   const factory Post({
+    required String id,
     required String authorId,
     required String fechaPublicacion,
     required String codeLanguage,
     required String codeSnippet,
-    required List<Comment> comments,
     required String description,
     required List<String> tags,
     @Default(0) int shares, // Como no viene, le damos 0 por defecto
