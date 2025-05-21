a294bf8e91a36ce9c52f7a7e0f687e21030a9ff3
SSAYKO <sandresmurillo@outlook.com>
2025-05-20 21:46:41 -0500

    Refactor: Update FeedController constructor formatting and enhance UsersProfileView to use CustomScrollView for better layout management

--- devinsight/lib/ui/home/views/users_profile.dart
+++ devinsight/lib/ui/home/views/users_profile.dart
@@ -1,8 +1,9 @@
+import 'package:devinsight/controller/profileFeedController.dart';
 import 'package:devinsight/models/user/user.dart';
+import 'package:devinsight/ui/home/widgets/publicationsCard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../theme/app_colors.dart';
-import '../widgets/navProfile.dart';
 import '../widgets/profile/profile_header.dart';
 import '../widgets/profile/user_bio.dart';
 
@@ -13,18 +14,48 @@ class UsersProfileView extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final feedState = ref.watch(profileFeedControllerProvider);
+
     return Scaffold(
       appBar: _buildAppBar(),
       backgroundColor: AppColors.background,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            ProfileHeader(user: user),
-            const SizedBox(height: 10),
-            UserBio(bio: user.bio ?? "Sin biografía"),
-            const NavProfile(),
-          ],
-        ),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(child: ProfileHeader(user: user)),
+          const SliverToBoxAdapter(child: SizedBox(height: 10)),
+          SliverToBoxAdapter(child: UserBio(bio: user.bio ?? "Sin biografía")),
+          const SliverToBoxAdapter(child: SizedBox(height: 20)),
+
+          // Publicaciones:
+          feedState.when(
+            data: (posts) {
+              if (posts.isEmpty) {
+                return const SliverToBoxAdapter(
+                  child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Center(child: Text("Sin publicaciones")),
+                  ),
+                );
+              }
+
+              return SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) => PublicationsCard(post: posts[index]),
+                  childCount: posts.length,
+                ),
+              );
+            },
+            loading: () => const SliverToBoxAdapter(
+              child: Center(child: CircularProgressIndicator()),
+            ),
+            error: (error, _) => SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Text('Error al cargar publicaciones: $error'),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
